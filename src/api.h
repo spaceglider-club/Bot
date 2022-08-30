8540ceedb55456f8d48d70672b0b0972ccd0d2fc
souljah <70041575+souljahwitch@users.noreply.github.com>
2022-08-30 06:51:18 +0200

    fixed line endings 🤓

--- src/api.h
+++ src/api.h
@@ -14,20 +14,20 @@ namespace API
 	{
 		UNKNOWN = 0,
 		PURPLE	= 1,
-		RED		= 2
+		RED	= 2
 	};
 
 	struct LocalPlayer
 	{
-		PTR address								= 0x0;
-		float attack_speed						= 0.0f;
-		float total_ad							= 0.0f;
-		float armor_penetration					= 0.0f;
-		float lethality							= 0.0f;
-		bool is_alive							= false;
-		TEAM team								= TEAM::UNKNOWN;
-		Vector::Vector3<float> world_position	= { 0.0f, 0.0f, 0.0f };
-		Vector::Vector2<float> screen_position	= { 0.0f, 0.0f };
+		PTR address	= 0x0;
+		float attack_speed = 0.0f;
+		float total_ad = 0.0f;
+		float armor_penetration	= 0.0f;
+		float lethality	= 0.0f;
+		bool is_alive = false;
+		TEAM team = TEAM::UNKNOWN;
+		Vector::Vector3<float> world_position = { 0.0f, 0.0f, 0.0f };
+		Vector::Vector2<float> screen_position = { 0.0f, 0.0f };
 	};
 
 	// For the sole purpose of performance, we are not using
@@ -35,17 +35,17 @@ namespace API
 
 	struct EnemyPlayer
 	{
-		PTR address								= 0x0;
-		std::string champion_name				= "";
-		float health							= 0.0f;
-		float armor								= 0.0f;
-		bool is_alive							= false;
-		bool is_targetable						= false;
-		bool is_visible							= false;
-		bool is_vulnerable						= false;
-		TEAM team								= TEAM::UNKNOWN;
-		Vector::Vector3<float> world_position	= { 0.0f, 0.0f, 0.0f };
-		Vector::Vector2<float> screen_position	= { 0.0f, 0.0f };
+		PTR address	= 0x0;
+		std::string champion_name = "";
+		float health = 0.0f;
+		float armor = 0.0f;
+		bool is_alive = false;
+		bool is_targetable = false;
+		bool is_visible = false;
+		bool is_vulnerable = false;
+		TEAM team = TEAM::UNKNOWN;
+		Vector::Vector3<float> world_position = { 0.0f, 0.0f, 0.0f };
+		Vector::Vector2<float> screen_position = { 0.0f, 0.0f };
 	};
 
 	void LoadLocalPlayer(LocalPlayer &local_player);
--- src/offsets.h
+++ src/offsets.h
@@ -9,36 +9,36 @@ namespace Offsets
 {
 	// Main Constants
 
-	inline constexpr PTR kLocalPlayer			= 0x3118DDC;
-	inline constexpr PTR kViewProjectionMatrix	= 0x3148A20;
-	inline constexpr PTR kGameTime				= 0x3111E68;
+	inline constexpr PTR kLocalPlayer = 0x3118DDC;
+	inline constexpr PTR kViewProjectionMatrix = 0x3148A20;
+	inline constexpr PTR kGameTime = 0x3111E68;
 
 	// Renderer Constants
 
-	inline constexpr PTR kRenderer				= 0x314B90C;
-	inline constexpr PTR kRendererWidth			= 0x8;
-	inline constexpr PTR kRendererHeight		= 0xC;
+	inline constexpr PTR kRenderer = 0x314B90C;
+	inline constexpr PTR kRendererWidth	= 0x8;
+	inline constexpr PTR kRendererHeight = 0xC;
 
 	// List Constants
 
-	inline constexpr PTR kHeroList				= 0x187BF54;
-	inline constexpr PTR kMinionList			= 0x24C9788;
+	inline constexpr PTR kHeroList = 0x187BF54;
+	inline constexpr PTR kMinionList = 0x24C9788;
 
 	// Champion Constants
 
-	inline constexpr PTR kChampionName			= 0x2BD4;
-	inline constexpr PTR kAttackRange			= 0x139C;
-	inline constexpr PTR kHealth				= 0xE74;
-	inline constexpr PTR kBaseAD				= 0x1354;
-	inline constexpr PTR kBonusAD				= 0x12CC;
-	inline constexpr PTR kArmorPenetration		= 0x1274;
-	inline constexpr PTR kLethality				= 0x128C;
-	inline constexpr PTR kIsTargetable			= 0xD04;
-	inline constexpr PTR kIsVisible				= 0x274;
-	inline constexpr PTR kIsVulnerable			= 0x3D4;
-	inline constexpr PTR kTeam					= 0x34;
-	inline constexpr PTR kPosition				= 0x1DC;
-	inline constexpr PTR kArmor					= 0x137C;
+	inline constexpr PTR kChampionName = 0x2BD4;
+	inline constexpr PTR kAttackRange = 0x139C;
+	inline constexpr PTR kHealth = 0xE74;
+	inline constexpr PTR kBaseAD = 0x1354;
+	inline constexpr PTR kBonusAD = 0x12CC;
+	inline constexpr PTR kArmorPenetration = 0x1274;
+	inline constexpr PTR kLethality = 0x128C;
+	inline constexpr PTR kIsTargetable = 0xD04;
+	inline constexpr PTR kIsVisible = 0x274;
+	inline constexpr PTR kIsVulnerable = 0x3D4;
+	inline constexpr PTR kTeam = 0x34;
+	inline constexpr PTR kPosition = 0x1DC;
+	inline constexpr PTR kArmor = 0x137C;
 };
 
 
